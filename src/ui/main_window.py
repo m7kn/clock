cbcb44cb8bb2a30c4f1daf248c07eb6bf710f38a
m7kn <m7kn1872@gmail.com>
2024-10-09 20:36:13 +0200

    add resizable option in frameless mode

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QMenu, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QMenu, QMessageBox, QSizeGrip
 from PyQt5.QtCore import Qt, QPoint, QRect
 from PyQt5.QtGui import QColor, QGuiApplication
 from .clock_widget import ClockWidget
@@ -15,6 +15,8 @@ class MainWindow(QMainWindow):
         self.draggable = False
         self.offset = QPoint()
         self.setAttribute(Qt.WA_TranslucentBackground)
+        self.resizable = False
+        self.size_grip = None
         self.init_ui()
     
     def init_ui(self):
@@ -67,16 +69,43 @@ class MainWindow(QMainWindow):
         self.main_layout.setContentsMargins(background_margin, background_margin, background_margin, background_margin)
         self.clock_widget.update_config()
         self.show()  # Necessary to apply changes
+        
+        # Update cursor based on frameless mode and resizability
+        self.update_cursor()
+        
+        # Update size grip visibility
+        self.update_size_grip()
+
+    def update_cursor(self):
+        """Update the cursor based on the current window state."""
+        config = self.config_manager.get_config()
         if config['main_window']['frameless']:
-            self.setCursor(Qt.OpenHandCursor)  # Change cursor to indicate movability
+            self.setCursor(Qt.OpenHandCursor if not self.resizable else Qt.ArrowCursor)
         else:
-            self.setCursor(Qt.ArrowCursor)  # Reset to default cursor
+            self.setCursor(Qt.ArrowCursor)
+
+    def update_size_grip(self):
+        """Update the size grip based on the current window state."""
+        if self.resizable:
+            if not self.size_grip:
+                self.size_grip = QSizeGrip(self)
+            self.size_grip.show()
+            self.size_grip.raise_()
+        else:
+            if self.size_grip:
+                self.size_grip.hide()
 
     def show_context_menu(self, position):
         """Show the context menu when right-clicking on the window."""
         menu = QMenu(self)
         settings_action = menu.addAction("Settings")
         about_action = menu.addAction("About")
+        
+        # Add toggle resize action
+        config = self.config_manager.get_config()
+        if config['main_window']['frameless']:
+            resize_action = menu.addAction("Enable Resizing" if not self.resizable else "Disable Resizing")
+        
         exit_action = menu.addAction("Exit")
         
         action = menu.exec_(self.mapToGlobal(position))
@@ -84,8 +113,24 @@ class MainWindow(QMainWindow):
             self.open_settings()
         elif action == about_action:
             self.show_about_dialog()
+        elif config['main_window']['frameless'] and action == resize_action:
+            self.toggle_resize()
         elif action == exit_action:
             self.close()
+
+    def toggle_resize(self):
+        """Toggle the resizability of the window."""
+        config = self.config_manager.get_config()
+        
+        if config['main_window']['frameless']:
+            self.resizable = not self.resizable            
+            if self.resizable:
+                self.setWindowFlags(self.windowFlags() & ~Qt.FramelessWindowHint)
+            else:
+                self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        
+            self.update_style()
+            self.show()  # Necessary to apply changes after updating window flags
 
     def open_settings(self):
         """Open the settings window."""
@@ -102,11 +147,12 @@ class MainWindow(QMainWindow):
         if event.button() == Qt.LeftButton:
             self.draggable = True
             self.offset = event.pos()
-            self.setCursor(Qt.ClosedHandCursor)  # Change cursor to indicate grabbing
+            if self.config_manager.get_config()['main_window']['frameless'] and not self.resizable:
+                self.setCursor(Qt.ClosedHandCursor)  # Change cursor to indicate grabbing
 
     def mouseMoveEvent(self, event):
         """Handle mouse move events for window dragging."""
-        if self.draggable and self.config_manager.get_config()['main_window']['frameless']:
+        if self.draggable and self.config_manager.get_config()['main_window']['frameless'] and not self.resizable:
             config = self.config_manager.get_config()
             corner_radius = config['main_window']['corner_radius']
             background_margin = config['main_window']['background_margin']
@@ -128,10 +174,7 @@ class MainWindow(QMainWindow):
         """Handle mouse release events for window dragging."""
         if event.button() == Qt.LeftButton:
             self.draggable = False
-            if self.config_manager.get_config()['main_window']['frameless']:
-                self.setCursor(Qt.OpenHandCursor)  # Change cursor back to indicate movability
-            else:
-                self.setCursor(Qt.ArrowCursor)  # Reset to default cursor
+            self.update_cursor()
 
     def closeEvent(self, event):
         """Handle the window close event."""
@@ -146,4 +189,3 @@ class MainWindow(QMainWindow):
         geometry = config['main_window']['geometry']
         if geometry:
             self.setGeometry(QRect(*geometry))
-            
